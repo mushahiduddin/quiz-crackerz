16dad1eb151aa8082f87d8e87a75b1fdff8db269
unknown <moshahiduddin@gmail.com>
2022-10-25 12:50:39 +0600

    in eye button correct answer showing by a toast

--- src/Components/SingleQuiz/SingleQuiz.js
+++ src/Components/SingleQuiz/SingleQuiz.js
@@ -6,21 +6,23 @@ const SingleQuiz = ({ singleQuiz }) => {
     const { question, options, correctAnswer } = singleQuiz;
     console.log(correctAnswer);
     const notify = (e) => {
-        if (e.target.innerText.toLowerCase() === correctAnswer.toLowerCase() ) {
-            console.log(correctAnswer);
-            console.log(e.target.innerText);
+        if (e.target.innerText === correctAnswer.replace(/\s+/g, ' ').trim()) {
             toast("Wow Genious !! Your answer Is Correct. ");
         }
         else {
             toast("That's Wrong !!");
         }
     };
+
+    const handleCorrectAnswer =() =>{
+        toast(correctAnswer.replace(/\s+/g, ' ').trim());
+    }
     return (
-        <div className='border border-amber-900 rounded-xl shadow-2xl p-4 m-8 w-11/12 mx-auto text-left'>
-            <div className='flex justify-between'><h3 className='text-xl font-bold w-9/12'>Question : {question.slice(3,-4)}</h3>
-            <p className=''><i className="fa-solid fa-eye"></i></p>
+        <div className='border border-amber-900 rounded-xl shadow-2xl p-4 m-8 w-9/12 mx-auto text-left'>
+            <div className='flex justify-between'><h3 className='text-xl font-bold w-11/12'>Question : {question.slice(3,-4)}</h3>
+            <p onClick={handleCorrectAnswer} className=''><i className="fa-solid fa-eye"></i></p>
             </div>
-            <div className='grid grid-cols-2 gap-4 my-4'>
+            <div className='grid grid-cols-1 md:grid-cols-2 gap-4 my-4'>
                 <p className='border border-emerald-900 rounded-xl shadow-xl p-2'>1. <span onClick={notify}>{options[0]}</span></p>
                 <p className='border border-emerald-900 rounded-xl shadow-xl p-2'>2. <span onClick={notify}>{options[1]}</span></p>
                 <p className='border border-emerald-900 rounded-xl shadow-xl p-2'>3. <span onClick={notify}>{options[2]}</span></p>
